ef2242d623efb85862481f3ed01315b97023690c
Carina Payleman <rociocarina2@gmail.com>
2023-06-26 23:40:50 -0300

    Crea interfaz skill para typear hard & soft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Skill } from './models/skill';
 
 @Component({
   selector: 'app-root',
--- src/app/models/skill.ts
+++ src/app/models/skill.ts
@@ -1,5 +1,10 @@
 export interface Skill {
+<<<<<<< HEAD
   id: number;
   name: string;
   icon?: string;
+=======
+  name: string;
+  icon: string;
+>>>>>>> 50b875f (Crea interfaz skill para typear hard & soft)
 }
