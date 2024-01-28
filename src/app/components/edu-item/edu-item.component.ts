d7c8ebe3acf398dab7458136cf8f9ee39db0741c
kodali.sai.teja <kodali.sai.teja@accenture.com>
2024-01-28 03:09:43 -0500

    changes

--- src/app/components/edu-item/edu-item.component.ts
+++ src/app/components/edu-item/edu-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Institute } from 'src/app/models/institute';
+import { Experience } from 'src/app/models/experience';
 
 @Component({
   selector: 'app-edu-item',
@@ -17,18 +18,33 @@ export class EduItemComponent {
       {
         "id": 0,
         "date": "",
-        "title": "",
-        "description": []
-      }
+        "title": ""      }
     ]
+  }
+
+  @Input() experience: Experience = {
+    "id": 0,
+      "company": "",
+      "title": "",
+      "date": "",
+      "description": "",
+      "responsibilities": []
   }
 
   get courses() {
     return this.education.courses;
   }
 
+  get experiences(){
+    return this.experience;
+  }
+
   getClass() {
     let institute = this.education.institute;
     return institute.replace(/\s+/g, '-').toLowerCase();
+  }
+  getClassExp(){
+    let company = this.experience.company;
+    return company.replace(/\s+/g, '-').toLowerCase();
   }
 }
